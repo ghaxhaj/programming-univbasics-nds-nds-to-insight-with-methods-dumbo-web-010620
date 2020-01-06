e3c3b4d299c7aa8222f1a0d7b83a1fe56b15e982
wise-integer-6834 <ghaxhaj@fordham.edu>
2020-01-06 01:51:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,8 +4,17 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+total = 0
+index = 0
+
+while index < director_data.length do
+  total += director_data[index][:worldwide_grosses]
+  index += 1
 
 end
+
+total
+end 
 
 # Write a method that, given an NDS creates a new Hash
 # The return value should be like:
