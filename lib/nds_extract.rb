6aab40c392d941c9eadbb7f85cee94f9903af31c
wise-integer-6834 <ghaxhaj@fordham.edu>
2020-01-06 01:53:17 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,7 +14,7 @@ while index < director_data.length do
 end
 
 total
-end 
+end
 
 # Write a method that, given an NDS creates a new Hash
 # The return value should be like:
@@ -22,11 +22,11 @@ end
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
   result = {}
-  director_index = 0 
-  while director_index < nds.length do 
+  director_index = 0
+  while director_index < nds.length do
     director = nds[director_index]
     result[director[:name]] = gross_for_director(director)
-    director_index += 1 
-  end 
+    director_index += 1
+  end
   result
 end
